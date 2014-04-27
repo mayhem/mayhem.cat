dbf16aaa7351fe6c58b7b913513a63b012656f41
Robert Kaye <rob@musicbrainz.org>
2014-04-27 20:46:21 +0200

    Fixed some resize issues and implemented dynamic image urls

--- mayhem_cat/server.py
+++ mayhem_cat/server.py
@@ -17,11 +17,23 @@ app = Flask(__name__,
 def index():
     try:
         with open(os.path.join(config.CACHE_DIR, "twat"), "r") as f:
-            twat = f.read()
+            twat = f.read().strip()
     except IOError:
         twat = "No tweets! What??"
 
-    return render_template("index", twat = twat)
+    try:
+        with open(os.path.join(config.CACHE_DIR, "mayhem_url"), "r") as f:
+            mayhem_url = f.read().strip()
+    except IOError:
+        mayhem_url = ""
+
+    try:
+        with open(os.path.join(config.CACHE_DIR, "hair_url"), "r") as f:
+            hair_url = f.read().strip()
+    except IOError:
+        hair_url = ""
+
+    return render_template("index", twat = twat, hair_url = hair_url, mayhem_url = mayhem_url)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
