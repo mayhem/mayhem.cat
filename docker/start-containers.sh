8cc6b29d69cceee2730fb23683a4f9d92668c2cf
Robert Kaye <rob@musicbrainz.org>
2019-07-16 13:00:18 +0200

    Update JS depedencies to use a CDN

--- docker/start-containers.sh
+++ docker/start-containers.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+docker network create website-network
+
 docker run -d \
     --expose 3031 \
     --name mayhem.cat \
-    -v /home/website/mayhem.cat:/code/mayhem.cat \
+    -v `pwd`/..:/code/mayhem.cat \
     --env "VIRTUAL_HOST=mayhem.cat" \
     --env "LETSENCRYPT_HOST=mayhem.cat" \
     --env "LETSENCRYPT_EMAIL=mayhem@gmail.com" \
     --network=website-network \
-    stash:beta
+    mayhem-cat:beta
